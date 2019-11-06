4c5615272ef3974f65f6fd374e00255da671278e
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-11-06 13:42:57 -0600

    setting up views

--- myEbag/views.py
+++ myEbag/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Items
+
+def item_list(request):
+    items = Items.objects.all()
+    return render(request, 'Ebag/item_list.html', {'items': items})
