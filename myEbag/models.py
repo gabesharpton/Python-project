90b4fea86c69ef618751c942968e3048b0fcd945
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-11-06 12:08:09 -0600

    admin site up and running

--- myEbag/models.py
+++ myEbag/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Items(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
